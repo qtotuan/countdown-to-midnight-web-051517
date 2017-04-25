470bb7a9b5467b6f18ff8be20aa6e1e9dea1fce4
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-25 23:03:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    i = n
+    while i > 0 do
+      puts "#{i} SECOND(S)!"
+      i -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  i = n
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
